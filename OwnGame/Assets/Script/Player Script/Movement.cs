91ba5ee875f0c40616081071b355b09ebc55717b
CryzerFranz <67900397+CryzerFranz@users.noreply.github.com>
2020-07-06 17:02:37 +0200

    Player movement and add some walls in area
    Add some walls in test area and baked a nav mesh to move with the player ( player movement ) camera is following the player at the moment

---
+++ OwnGame/Assets/Script/Player Script/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    //To set animation
+    //private Animator playerAnimator;
+    
+    private NavMeshAgent playerNavMeshAgent;
+    private Camera playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerAnimator = GetComponent<Animator>();
+        playerNavMeshAgent = GetComponent<NavMeshAgent>();
+        playerCamera = GameObject.Find("Player_Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            //keyword out because hit isn't assigned, for people who not remember how to code in C# kappa
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                playerNavMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
