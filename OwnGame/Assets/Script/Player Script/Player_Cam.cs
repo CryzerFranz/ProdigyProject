e6ae89adc39d363ce66d4d9b7e694b814bb651c8
CryzerFranz <67900397+CryzerFranz@users.noreply.github.com>
2020-07-07 00:10:40 +0200

    Player Camera
    Add script for player camera.
    camera follow player, do not turn 360°
    camera should be static looking to the front

---
+++ OwnGame/Assets/Script/Player Script/Player_Cam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Cam : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    private Vector3 cameraOffset;
+
+    public float SmoothFactor = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = playerTransform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+}
