99e73051d0a5b8a5c49fe992daa4b6ecf7a79dbe
Hannomat <h4nnom4t@gmail.com>
2020-07-08 19:51:51 +0200

    WIP: Stats

---
+++ OwnGame/Assets/Script/Stats/StatInteface.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats 
+{
+    float LifePoints { get; set; }
+    float Defense { get; set; }
+    float CritChance { get; set; }
+    float Dexterity { get; set; }
+    float Strength { get; set; }
+}
