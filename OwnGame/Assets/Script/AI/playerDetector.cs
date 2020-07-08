9cf6bbcd0b80a9653d4685f4b3a44d433c6c898c
CryzerFranz <67900397+CryzerFranz@users.noreply.github.com>
2020-07-08 12:57:03 +0200

    improved AI movement script
    AI can no more see player through the wall. view angle and view radius of enemy is adjustable

--- OwnGame/Assets/Script/AI/playerDetector.cs
+++ OwnGame/Assets/Script/AI/playerDetector.cs
@@ -8,33 +8,100 @@ using UnityEngine.AI;
 public class playerDetector : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject player;
-    private bool playerDetected = false;
     private NavMeshAgent enemyNavMeshAgent;
 
 
+    public Transform player;
 
-    void Start()
+    [Range(2.0f, 360f)]
+    public float maxAngle;
+   
+    public float maxRadius;
+
+    private bool isInFOV = false;
+
+    //just drawing lines to show
+    private void OnDrawGizmos()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        enemyNavMeshAgent = GameObject.Find("Enemy Dummy").GetComponent<NavMeshAgent>();
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
 
+        Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, fovLine1);
+        Gizmos.DrawRay(transform.position, fovLine2);
+
+
+        //line between the player and the enemy
+        if(!isInFOV)
+        {
+            Gizmos.color = Color.cyan;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+        }
+        Gizmos.DrawRay(transform.position, (player.position - transform.position).normalized * maxRadius );
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //checking if player is in the Radius and viewAngle of the enemy
+    public static bool inFOV(NavMeshAgent enemyToTarget, Transform checkingObject, Transform target, float maxAngle, float maxRadius)
     {
-        if (playerDetected)
+        //Object in the radius are collected in this Collider Array
+        Collider[] overlaps = new Collider[20];
+        int count = Physics.OverlapSphereNonAlloc(checkingObject.position, maxRadius, overlaps);
+
+        for (int i = 0; i < count; i++)
         {
-            enemyNavMeshAgent.destination = player.transform.position;
+            if(overlaps[i] != null)
+            {
+                //checks if the current object ( overlaps[i]) position is equal to the player position
+                if(overlaps[i].transform == target)
+                {
+                    //calculate the position between the objects
+                    Vector3 directionBetween = (target.position - checkingObject.position).normalized;
+                    //the y axis postion isn't important 
+                    directionBetween.y *= 0;
+
+
+                    float angle = Vector3.Angle(checkingObject.forward, directionBetween);
+
+
+                    if(angle <= maxAngle)
+                    {
+                        //We are in the view angle of the AI
+
+                        Ray ray = new Ray(checkingObject.position, target.position - checkingObject.position);
+                        RaycastHit hit;
+
+                        if (Physics.Raycast(ray, out hit, maxRadius))
+                        {
+                            if (hit.transform == target)
+                            {
+                                enemyToTarget.SetDestination(target.position);
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
+        return false;
     }
-    void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.gameObject.tag == "Player")
-        {
-            playerDetected = true;
-        }
+        isInFOV = inFOV(enemyNavMeshAgent, transform, player, maxAngle, maxRadius);
+    }
+
+    private void Start()
+    {
+        enemyNavMeshAgent = GetComponent<NavMeshAgent>();
     }
 }
