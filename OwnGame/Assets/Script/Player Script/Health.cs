ff04deaf80706d7b0587ec5d03e438ae6aa8338c
Christophe <cp.paleyron@gmail.com>
2020-07-29 13:53:53 +0200

    Fixing player death
    Player cant no more move if he died and animation is playing just one time

--- OwnGame/Assets/Script/Player Script/Health.cs
+++ OwnGame/Assets/Script/Player Script/Health.cs
@@ -7,15 +7,29 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
 
-    private PlayerStats playerStats;
     public Healthbar healthbar;
+
+    private PlayerStats playerStats;
+    private Animator animator;
 
     private float maxHealth;
     private float currentHealth;
 
+    private bool isPlayerDead = false;
+
+    public bool IsPlayerDead
+    {
+        get
+        {
+            return currentHealth <= 0;
+        }
+    }
+
     void Start()
     {
+        animator = GetComponent<Animator>();
         playerStats = GetComponent<PlayerStats>();
+
         maxHealth = playerStats.LifePoints;
         currentHealth = maxHealth;
         healthbar.SetMaxHealth(maxHealth);
@@ -23,14 +37,21 @@ public class Health : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        ///// TESTPHASE
-        damage -= (damage * (playerStats.Defense * 0.1f));
-        ///// TESTPHASE
-        currentHealth -= damage;
-        healthbar.SetHealth(currentHealth);
+        if(!IsPlayerDead)
+        {
+            ///// TESTPHASE
+            damage -= (damage * (playerStats.Defense * 0.1f));
+            ///// TESTPHASE
+
+            currentHealth -= damage;
+            healthbar.SetHealth(currentHealth);
+            if (IsPlayerDead)
+            {
+                animator.SetTrigger("isDead");
+            }
+        }
+       
     }
-
-
 
     public float MaxHealth
     {
