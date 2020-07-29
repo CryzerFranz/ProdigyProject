611c2edeaf49fc30ebbab97d02e2448ae6548285
Christophe <cp.paleyron@gmail.com>
2020-07-29 22:45:21 +0200

    add new UI elements + animation

---
+++ OwnGame/Assets/Script/Player Script/test_combat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_combat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.SetTrigger("Melee_Spalten");
+        }
+    }
+}
