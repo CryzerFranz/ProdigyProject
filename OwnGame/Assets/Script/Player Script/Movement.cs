7ba3fd10a1715f7fd58556f8d7c7a25af4b8f0d0
Christophe <cp.paleyron@gmail.com>
2020-08-08 15:30:21 +0200

    Target enemy ( some issues included ) )

--- OwnGame/Assets/Script/Player Script/Movement.cs
+++ OwnGame/Assets/Script/Player Script/Movement.cs
@@ -7,14 +7,17 @@ using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
-
     private NavMeshAgent playerNavMeshAgent;
     private Health health;
     private Animator animator;
     private Camera playerCamera;
+
+    int layer_mask;
+    bool isFollowTarget = false;
    
     void Start()
     {
+        layer_mask = LayerMask.GetMask("enemy");
         playerNavMeshAgent = GetComponent<NavMeshAgent>();
         health = GetComponent<Health>();
         animator = GetComponent<Animator>();
@@ -29,8 +32,20 @@ public class Movement : MonoBehaviour
             Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
+            if (Input.GetMouseButtonDown(1))
+            {
+                Debug.Log("RightCLick");
+                if (Physics.Raycast(ray, out hit, 100, layer_mask))
+                {
+                    isFollowTarget = true;
+                    animator.SetFloat("Forward", 2f);
+                    playerNavMeshAgent.SetDestination(hit.point);
+                }
+            }
+
             if (Input.GetMouseButtonDown(0))
             {
+                isFollowTarget = false;
                 if (Physics.Raycast(ray, out hit, 100))
                 {
                     animator.SetFloat("Forward", 2f);
@@ -39,11 +54,23 @@ public class Movement : MonoBehaviour
             }
             else
             {
+                
                 float distanceRemaining = playerNavMeshAgent.remainingDistance;
                 if (distanceRemaining != Mathf.Infinity && playerNavMeshAgent.pathStatus == NavMeshPathStatus.PathComplete && playerNavMeshAgent.remainingDistance == 0)
                 {
                     animator.SetFloat("Forward", 0f);
-                    playerNavMeshAgent.ResetPath();
+
+                    if (!isFollowTarget)
+                    {
+                        playerNavMeshAgent.ResetPath();
+                    }
+                }
+                else
+                {
+                    if (isFollowTarget)
+                    {
+                        animator.SetFloat("Forward", 2f);
+                    }
                 }
             }
         }
