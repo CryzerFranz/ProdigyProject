d39f0918a2cc27d953a59c7b9e7eeb5b15995158
Christophe <cp.paleyron@gmail.com>
2020-09-09 19:39:06 +0200

    targeting enemy with TAB

--- OwnGame/Assets/Script/Game Manager/EnemySpawner.cs
+++ OwnGame/Assets/Script/Game Manager/EnemySpawner.cs
@@ -11,7 +11,7 @@ public class EnemySpawner : MonoBehaviour
     
     public int enemyCount = 1;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         for (int i = 0; i < enemyCount; i++)
         {
