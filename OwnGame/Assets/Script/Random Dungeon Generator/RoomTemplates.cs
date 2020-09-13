6ec6e643ff59f1aae401bd614ebddad3d523e5d7
Christophe <cp.paleyron@gmail.com>
2020-09-13 20:45:25 +0200

    dungeon generator ( not finished )

---
+++ OwnGame/Assets/Script/Random Dungeon Generator/RoomTemplates.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+
+    public GameObject closedRoom;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
