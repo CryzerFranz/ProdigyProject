d4ecfdb556b436b2b79d1a741148d275d7fa664e
Christophe <cp.paleyron@gmail.com>
2020-10-16 19:06:32 +0200

    TestScript Folder, Test Animation folder, new model

---
+++ OwnGame/Assets/Script/TestScripts/Interact_Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    public Transform player;
+    public float interactRadius = 5f;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Physics.CheckSphere(transform.position, interactRadius, 1 << 8))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("Entry");
+            }
+        }
+    }
+
+
+}
