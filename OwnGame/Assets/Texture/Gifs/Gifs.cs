c2c9d0f7dfbe69955ac10775ca3ef4a4d3435b08
marcel <strengmarcel@web.de>
2020-11-18 20:42:00 +0100

    Details + Clup und Bar errichten

---
+++ OwnGame/Assets/Texture/Gifs/Gifs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.IO;
+using UnityEngine.Video;
+using System.Linq;
+
+public class Gifs : MonoBehaviour
+{
+    UnityEngine.Object[] textures;
+    int CurrentFrame = 1;
+
+    [SerializeField]
+    string folderpath;
+
+    private void Start()
+    {
+        textures = Resources.LoadAll(folderpath, typeof(Texture)).Cast<Texture>().ToArray();
+        Debug.Log(folderpath);
+
+        foreach(var t in textures)
+        {
+            Debug.Log(t.name);
+        }
+    }
+
+    private void Update()
+    {
+        //CurrentFrame %= textures.Length;
+        //CurrentFrame++;
+        //GetComponent<Renderer>().material.mainTexture = textures[CurrentFrame];
+    }
+
+
+
+
+
+}
