d0dd93c7d781ee7ab05505166df3b56d58880cc5
Christophe <cp.paleyron@gmail.com>
2020-11-19 18:00:00 +0100

    Soda  glitch ready + script

---
+++ OwnGame/Assets/Texture/PNGs/Gifs/Gif_Maker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gif_Maker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Texture[] textures;
+    int framesPerSecond = 10;
+    void Start()
+    {
+        textures = Resources.LoadAll<Texture>("Glitched_01") ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = Convert.ToInt32(Time.time * framesPerSecond);
+        index = index % textures.Length;
+        GetComponent<Renderer>().material.mainTexture = textures[index];
+    }
+
+}
