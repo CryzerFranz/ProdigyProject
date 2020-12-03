ccbd3cc9ecdb479c19f3aaad161ad626b2507564
Christophe <cp.paleyron@gmail.com>
2020-12-03 12:44:57 +0100

    Animationen von paar Objekten

---
+++ OwnGame/Assets/Script/Animation Script/Rotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Rotation")]
+    [SerializeField]
+    private float rotateSpeedX;
+    [SerializeField]
+    private float rotateSpeedY;
+    [SerializeField]
+    private float rotateSpeedZ;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotateSpeedX, rotateSpeedY, rotateSpeedZ));
+    }
+}
