a61e2eb5316d57dd7318487be688635a11a9891a
marcel <strengmarcel@web.de>
2020-12-09 17:44:48 +0100

    New PathCreator for NPC_Path

---
+++ OwnGame/Assets/Script/TestScripts/PathCreator_FollowLinesTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+
+public class PathCreator_FollowLinesTest : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 2;
+    public PathCreator path;
+    float distance;
+
+    void Update()
+    {
+        distance += speed * Time.deltaTime;
+        transform.position = path.path.GetPointAtDistance(distance);
+        transform.rotation = path.path.GetRotationAtDistance(distance);
+    }
+}
