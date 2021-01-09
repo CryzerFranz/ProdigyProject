e4e117554edd08c51a0a96114381bead54c1767e
Christophe <cp.paleyron@gmail.com>
2021-01-09 17:19:44 +0100

    anpassung des neues Rigs mit neuer kampf system

--- OwnGame/Assets/Script/Game Manager/EnemyManager.cs
+++ OwnGame/Assets/Script/Game Manager/EnemyManager.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     protected static bool sawPlayer = false;
+
+    public static EnemyManager instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public Attacking Basic_Psycho_attacking_Script;
 }
