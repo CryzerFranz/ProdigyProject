c4186bad03bb2a57af698d4b8232a69b98056138
Christophe <cp.paleyron@gmail.com>
2021-02-11 15:40:23 +0100

    update player.prefab

--- OwnGame/Assets/Script/Canvas_Manager.cs
+++ OwnGame/Assets/Script/Canvas_Manager.cs
@@ -29,37 +29,29 @@ public class Canvas_Manager : ScriptableObject
     {
         for (int i = 0; i < mainCanvas.transform.childCount; i++)
         {
-            //allCanvas[i] = mainCanvas.transform.GetChild(i).gameObject;
-            Debug.Log(mainCanvas.transform.GetChild(i).name);
             switch (mainCanvas.transform.GetChild(i).name)
             {
                 case "PlayerUI":
-                    Debug.Log(true);
                     Canvas_Player_UI = mainCanvas.transform.GetChild(i).gameObject;
                     Canvas_Player_UI.SetActive(true);
                     break;
                 case "FrameRate_Canvas":
-                    Debug.Log(true);
                     Canvas_FrameRate = mainCanvas.transform.GetChild(i).gameObject;
                     Canvas_FrameRate.SetActive(true);
                     break;
                 case "Canvas_Dead_Transition":
-                    Debug.Log(true);
                     Canvas_Dead_Transition = mainCanvas.transform.GetChild(i).gameObject;
                     Canvas_Dead_Transition.SetActive(false);
                     break;
                 case "Pause_Menue":
-                    Debug.Log(true);
                     Canvas_Pause_Menue = mainCanvas.transform.GetChild(i).gameObject;
                     Canvas_Pause_Menue.SetActive(false);
                     break;
                 case "Settings":
-                    Debug.Log(true);
                     Canvas_Settings = mainCanvas.transform.GetChild(i).gameObject;
                     Canvas_Settings.SetActive(false);
                     break;
                 case "KeyBoard_Settings":
-                    Debug.Log(true);
                     Canvas_KeyBoard_Settings = mainCanvas.transform.GetChild(i).gameObject;
                     Canvas_KeyBoard_Settings.SetActive(false);
                     break;
@@ -67,12 +59,5 @@ public class Canvas_Manager : ScriptableObject
         }
 
 
-    }
-
-    //private void OnEnable()
-    //{
-    //    
-    //}
-
-    
+    }   
 }
